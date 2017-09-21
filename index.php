146b8ffd3859459ae23347f40e342080e702c831
dlaredo <davidlaredo1@gmail.com>
2017-09-21 13:07:50 -0700

    Agregando paginacion

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
 <head>
   <link rel="stylesheet" href="tabla.css">
 
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twbs-pagination/1.3.1/jquery.twbsPagination.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.5/validator.min.js"></script>
+	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+        <link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
+
   <style>
 
 	/* unvisited link */
@@ -25,6 +33,74 @@
 	    color: blue;
 	}
 
+.rwd-table-2 {
+  margin: 1em 0;
+  min-width: 300px;
+}
+.rwd-table-2 tr {
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+}
+.rwd-table-2 th {
+  display: none;
+}
+.rwd-table-2 td {
+  display: block;
+}
+.rwd-table-2 td:first-child {
+  padding-top: .5em;
+}
+.rwd-table-2 td:last-child {
+  padding-bottom: .5em;
+}
+.rwd-table-2 td:before {
+  content: attr(data-th) ": ";
+  font-weight: bold;
+  width: 6.5em;
+  display: inline-block;
+}
+@media (min-width: 480px) {
+  .rwd-table-2 td:before {
+    display: none;
+  }
+}
+.rwd-table-2 th, .rwd-table-2 td {
+  text-align: left;
+}
+@media (min-width: 480px) {
+  .rwd-table-2 th, .rwd-table-2 td {
+    display: table-cell;
+    padding: .25em .5em;
+  }
+  .rwd-table-2 th:first-child, .rwd-table-2 td:first-child {
+    padding-left: 0;
+  }
+  .rwd-table-2 th:last-child, .rwd-table-2 td:last-child {
+    padding-right: 0;
+  }
+}
+
+
+.rwd-table-2 {
+  background: #34495E;
+  color: #fff;
+  border-radius: .4em;
+  overflow: hidden;
+}
+.rwd-table-2 tr {
+  border-color: #46637f;
+}
+.rwd-table-2 th, .rwd-table-2 td {
+  margin: .5em 1em;
+}
+@media (min-width: 480px) {
+  .rwd-table-2 th, .rwd-table-2 td {
+    padding: 1em !important;
+  }
+}
+.rwd-table-2 th, .rwd-table-2 td:before {
+  color: #dd5;
+}
   </style>
 
   <title>Centros de acopio sismo 2017</title>
@@ -37,58 +113,106 @@
 
 <?php
 
-	$dbConnection = null;
-	$fieldNamesCentroAcopio = array('Nombre del Centro', 'Calle', 'Numero', 'Colonia', 'Codigo Postal', 'Del/Mpio', 'Zona', 'Estado', 'Telefono', 'Contacto', 'Horarios', 'Tipo Centro', 'Mapa');
+    $dbConnection = null;
+    $fieldNamesCentroAcopio = array('Nombre del Centro', 'Calle', 'Numero', 'Colonia', 'Codigo Postal', 'Del/Mpio', 'Zona', 'Estado', 'Telefono', 'Contacto', 'Horarios', 'Tipo Centro', 'Mapa');
 
-	require 'api/db_config.php';
+    require 'api/db_config.php';
 
-	function createTable($tableName){
+    function createTable($tableName){
 
-		global $dbConnection, $fieldNamesCentroAcopio;
+        global $dbConnection, $fieldNamesCentroAcopio;
 
-		$queryStringGetAll = "SELECT * FROM " . $tableName;
+        $queryStringGetAll = "SELECT * FROM " . $tableName;
 
-		$mysql_resultGetAll = $dbConnection->query($queryStringGetAll);
-		$nEntries = $mysql_resultGetAll->num_rows;
-		$nFields = $mysql_resultGetAll->field_count;
+        $mysql_resultGetAll = $dbConnection->query($queryStringGetAll);
+        $nEntries = $mysql_resultGetAll->num_rows;
+        $nFields = $mysql_resultGetAll->field_count;
 
-		echo "<table class='rwd-table'>";
-		for ($row=0; $row <= $nEntries; $row++) {
+        echo "<table class='rwd-table hide'>";
+        for ($row=0; $row <= $nEntries; $row++) {
 
-			echo "<tr> \n";
-			if ($row==0){
-				for ($col=1; $col <= $nFields-1; $col++) { 
-					$p = $fieldNamesCentroAcopio[$col-1];
-				   	echo "<th>$p</th> \n";
-				}
-			}
-			else{
+            if ($row==0){
+              echo "<tr class='tab'> \n";
+                for ($col=1; $col <= $nFields-1; $col++) { 
+                    $p = $fieldNamesCentroAcopio[$col-1];
+                       echo "<th>$p</th> \n";
+                }
+            }
+            else{
+                echo "<tr> \n";
+                $resultRow = $mysql_resultGetAll->fetch_row();
 
-				$resultRow = $mysql_resultGetAll->fetch_row();
+                for ($col=1; $col <= $nFields-1; $col++) { 
+                   $p = $resultRow[$col];
+                   if ($col == 1){
+                       $ubicacion = $resultRow[2] . " " . $resultRow[3] . ", " . $resultRow[4] . ",  C.P. " .$resultRow[5]. ", " .$resultRow[6]. ", " .$resultRow[7];
+                       echo "<td><a href=\"crud_table.php?val=$resultRow[0]&name=$resultRow[1]&location=$ubicacion\" target=_blank>$p</a></td>\n";
+                   }
+                   else if ($col == $nFields-1){
+                       echo "<td><a href=$p target=_blank>Ver mapa</a></td>\n";
+                   }
+                   else{
+                       echo "<td>$p</td> \n";
+                   }
+                }
+            }
+              echo "</tr>";
+            }
+        echo "</table>";
+    }
 
-				for ($col=1; $col <= $nFields-1; $col++) { 
-				   $p = $resultRow[$col];
-				   if ($col == 1){
-				   	$ubicacion = $resultRow[2] . " " . $resultRow[3] . ", " . $resultRow[4] . ",  C.P. " .$resultRow[5]. ", " .$resultRow[6]. ", " .$resultRow[7];
-				   	echo "<td><a href=\"crud_table.php?val=$resultRow[0]&name=$resultRow[1]&location=$ubicacion\" target=_blank>$p</a></td>\n";
-				   }
-				   else if ($col == $nFields-1){
-				   	if ($p != ''){echo "<td><a href=$p target=_blank>Ver mapa</a></td>\n";}
-				   }
-				   else{
-				   	echo "<td>$p</td> \n";
-				   }
-				}
-			}
-		  	echo "</tr>";
-			}
-		echo "</table>";
-	}
-
-	$dbConnection = connectDB();
-	createTable("centro_acopio");
+    $dbConnection = connectDB();
+    createTable("centro_acopio");
 
 ?>
+<div class="fluid-container container-cancel">
+    <table class='rwd-table-2' align="center" cellpadding="1" cellspacing="1" style="vertical-align: text-top">
+        <tr>
+            <th>Nombre del Centro</th> 
+            <th>Calle</th> 
+            <th>Numero</th> 
+            <th>Colonia</th> 
+            <th>Codigo Postal</th> 
+            <th>Delegacion/Municipio</th>
+            <th>Zona</th> 
+            <th>Estado</th>
+            <th>Telefono</th> 
+            <th>Contacto</th> 
+            <th>Horarios</th>
+            <th>Tipo Centro</th>   
+            <th>Mapa</th> 
+        </tr>
+        <tbody id="container-page"></tbody>
+    </table>
+    <div class="panel-footer" align="center"><ul id="pagination-demo" class="pagination-sm"></ul></div>
+</div>
+
+<script>
+var elementosVisibles = 10;
+$(document).ready(function(){
+  setPagination();
+});
+ function setPagination(){
+  var totalElements = $(".rwd-table tbody tr:not(.tab)").length;
+  var paginasTotales = Math.ceil(totalElements / (elementosVisibles -1));
+  $("#container-page").html("");
+  if($('#pagination-demo').data("twbs-pagination")) $('#pagination-demo').twbsPagination('destroy');
+  $('#pagination-demo').twbsPagination({
+    totalPages: paginasTotales,
+    visiblePages: 7,
+    onPageClick: function (event, page) {
+      var inicio = ((elementosVisibles - 1) * page) - elementosVisibles + 1;
+      var final = inicio + elementosVisibles -1;
+      if(page < paginasTotales){
+        $("#container-page").html($(".rwd-table tbody tr:not(.tab)").slice(inicio, final).clone().addClass('listaPageShow  flipInX animated'));
+      }
+      else{
+        $("#container-page").html($(".rwd-table tbody tr:not(.tab)").slice(inicio,totalElements).clone().addClass('listaPageShow  flipInX animated'));
+      }
+    }
+  });
+}
+</script>
 
 </body>
 </html>
--- pruebas_sismo2017.sql
+++ pruebas_sismo2017.sql
@@ -1,9 +1,11 @@
 use id2987913_sismo2017acopios;
 
-INSERT INTO centro_acopio (Nombre, Calle, Colonia, Numero, CodigoPostal, Del_Municipio, Estado, URLMapa) VALUES ( 'Centro de Prueba 1', 'Calle de Prueba 1', 'Colonia de Prueba 1', 1, 1, 'Ecatepec', 'Estado de Mexico', 'https://www.google.com/maps/dir/37.3621424,-120.4241858//@37.3671052,-120.4184781,15z');
-INSERT INTO centro_acopio (Nombre, Calle, Colonia, Numero, CodigoPostal, Del_Municipio, Estado, URLMapa) VALUES ( 'Centro de Prueba 2', 'Calle de Prueba 2', 'Colonia de Prueba 2', 2, 2, 'GAM', 'Ciudad de Mexico', 'https://www.google.com/maps/dir/37.3621424,-120.4241858//@37.3671052,-120.4184781,15z');
-INSERT INTO centro_acopio (Nombre, Calle, Colonia, Numero, CodigoPostal, Del_Municipio, Estado, URLMapa) VALUES ( 'Centro de Prueba 3', 'Calle de Prueba 3', 'Colonia de Prueba 3', 3, 3, 'Cuahutemoc', 'Ciudad de Mexico', 'https://www.google.com/maps/dir/37.3621424,-120.4241858//@37.3671052,-120.4184781,15z');
-INSERT INTO centro_acopio (Nombre, Calle, Colonia, Numero, CodigoPostal, Del_Municipio, Estado, URLMapa) VALUES ( 'Centro de Prueba 4', 'Calle de Prueba 4', 'Colonia de Prueba 4', 4, 4, 'Acoxpa', 'Puebla', 'https://www.google.com/maps/dir/37.3621424,-120.4241858//@37.3671052,-120.4184781,15z');
+INSERT INTO centro_acopio (Nombre, Calle, Colonia, Numero, CodigoPostal, Zona, Estado, Telefono, Contacto, Horarios, TipoCentro, URLMapa) 
+VALUES ( 'Centro de Prueba 1', 'Calle de Prueba 1', 'Colonia de Prueba 1', 1, 1, 'Ecatepec', 'Estado de Mexico', '5548662066', 'Señor 1', '10 a 10', 'Cruz roja', 'https://www.google.com/maps/dir/37.3621424,-120.4241858//@37.3671052,-120.4184781,15z');
+
+INSERT INTO centro_acopio (Nombre, Calle, Colonia, Numero, CodigoPostal, Zona, Estado, Telefono, Contacto, Horarios, TipoCentro, URLMapa) 
+VALUES ( 'Centro de Prueba 2', 'Calle de Prueba 2', 'Colonia de Prueba 2', 1, 1, 'Mordor', 'CDMX', '5548662067', 'Señor 2', '10 a 10', 'Cruz roja', 'https://www.google.com/maps/dir/37.3621424,-120.4241858//@37.3671052,-120.4184781,15z');
+
 
 INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria) VALUES ( 1, 'Pilas', 20, 0, 'Misc');
 INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria) VALUES ( 1, 'Leche', 20, 0, 'Misc');
@@ -12,10 +14,10 @@ INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria)
 INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria) VALUES ( 1, 'Jabon', 20, 3, 'Misc');
 INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria) VALUES ( 1, 'Sabanas', 20, 1, 'Misc');
 INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria) VALUES ( 2, 'Pilas', 20, 0, 'Misc');
-INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria) VALUES ( 3, 'Leche', 20, 0, 'Misc');
-INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria) VALUES ( 4, 'Pañales', 20, 5, 'Misc');
+INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria) VALUES ( 2, 'Leche', 20, 0, 'Misc');
+INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria) VALUES ( 2, 'Pañales', 20, 5, 'Misc');
 INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria) VALUES ( 1, 'Sopa', 20, 2, 'Misc');
 INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria) VALUES ( 2, 'Jabon', 20, 3, 'Misc');
-INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria) VALUES ( 3, 'Sabanas', 20, 1, 'Misc');
+INSERT INTO articulos (centro_acopio_id, Nombre, Cantidad, Prioridad, Categoria) VALUES ( 2, 'Sabanas', 20, 1, 'Misc');
 
 INSERT INTO usuarios (username, pass, centroAcopioId) VALUES ('acopios', 'acopios', 1);
