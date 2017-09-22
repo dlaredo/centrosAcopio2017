ff4b0b69ecdcef995b06950a507c93a0a52bcf8d
Unknown <cordazsantiago@gmail.com>
2017-09-21 20:44:47 -0500

    se remueve el meta tag

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
   <link rel="stylesheet" href="tabla.css">
 
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
